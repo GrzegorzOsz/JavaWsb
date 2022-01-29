8858315926545da77de7508f99bd08558ba744af
grzes <grzes1508@tlen.pl>
2022-01-29 01:11:38 +0100

    Zadanie 1

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,8 +1,55 @@
 package com.company;
 
+import java.util.Objects;
+
 public class Animal {
     String species;
-    Double weight;
+    public Double weight;
     String name;
     String color;
+    Boolean alive;
+
+    public Animal(String species) {
+
+        this.species = species;
+
+
+        if (species.equals("canis")) {
+            this.weight = 10.0;
+        } else if (species.equals("fenis")) {
+            this.weight = 2.0;
+        } else{
+            this.weight = 1.0;
+        }
+
+
+
+    }
+
+    public Animal() {
+
+    }
+
+    public void feed() {
+        if (this.weight > 0) {
+
+            this.weight += 1;
+            System.out.println("waga: " + weight);
+        } else if (this.weight < 0) {
+            System.out.println("Karmić martwego ? " + species);
+            System.out.println("waga: " + weight);
+        }
+    }
+
+    public void takeForWalk() {
+        if (this.weight > 0) {
+
+            this.weight -= 1;
+            System.out.println("waga: " + weight);
+        } else if (this.weight <= 0) {
+            System.out.println("Martwego na spacer ?");
+            System.out.println("waga: " + weight);
+        }
+
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Animal dog = new Animal();
+        dog.species = "canis";
+        dog.weight = 12.0;
+        dog.name = "Szarik";
+
+        dog.feed();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.takeForWalk();
+        dog.feed();
 
     }
 }
