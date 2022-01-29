ada5a213482f488d13a57abe4761472788c66cc4
grzes <grzes1508@tlen.pl>
2022-01-29 02:47:23 +0100

    Zadanie 7

--- src/com/company/Device.java
+++ src/com/company/Device.java
@@ -3,19 +3,19 @@ package com.company;
 public abstract class Device {
     public final String producer;
     public final String model;
-    public final Integer yearOfProduction;
+    public final Integer yearOfProd;
 
-    public Device(String producer, String model, Integer yearOfProduction) {
+    public Device(String producer, String model, Integer yearOfProd) {
         this.producer=producer;
         this.model=model;
-        this.yearOfProduction=yearOfProduction;
+        this.yearOfProd=yearOfProd;
     }
 
     public String toString() {
         return "Device{" +
                 "producer='" + producer + '\'' +
                 ", model='" + model + '\'' +
-                ", yearOfProduction=" + yearOfProduction +
+                ", yearOfProduction=" + yearOfProd +
                 '}';
     }
 
