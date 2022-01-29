a27845b5966b1150f269e93bac82d03461aa1583
grzes <grzes1508@tlen.pl>
2022-01-29 01:55:31 +0100

    Zadanie 3

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,9 +1,29 @@
 package com.company;
 
+import java.util.Date;
+
 public class Human {
     String firstname;
     String lastName;
     Integer age;
-    Double salary;
+    private Double salary;
     private Car fiat;
+
+    Double getSalary() {
+        System.out.println("wyplata:"+this.salary);
+        System.out.println (new Date());
+        return salary;
+    }
+
+    void setSalary(double salary) {
+        if (salary < 0.00) {
+            System.out.println("Wypłata musi być wieksza niz 0");
+        } else {
+            System.out.println("Dane wysłane do ksiegowości");
+            System.out.println("Odbierz aneks do umowy z kadr");
+            System.out.println("Dane wysłane do ZUS i urzędu skarbowego");
+            this.salary = salary;
+        }
+
+    }
 }
