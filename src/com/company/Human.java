8121407740aa839669fa47e8299efbbc6f3191bd
grzes <grzes1508@tlen.pl>
2022-02-05 23:02:14 +0100

    Zadanie 11

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Human {
@@ -7,24 +8,12 @@ public class Human {
     String lastName;
     Integer age;
     private Integer salary;
-    private Car fiat;
+    private Double cash;
+    public Car[] garage;
+    private Object Car;
 
-    public Car getFiat() {
-        return this.fiat;
-    }
 
-    public void setFiat(Car fiat) {
-        if (this.salary > fiat.value) {
-            System.out.println("stać cie");
-        this.fiat = fiat;
-        }else if (this.salary > fiat.value/12 ) {
-            System.out.println ("udało się kupić samochód na kredyt");
-            this.fiat = fiat;
-        }else {
-            System.out.println("nie stać cie, poszukaj lepszej pracy");
-    }
-
-   public void getSalary() {
+    public Integer getSalary() {
         System.out.println("wyplata:"+this.salary);
         System.out.println (new Date());
         return salary;
@@ -41,17 +30,55 @@ public class Human {
         }
 
     }
-}
+
 
     public void setSalary(int salary) {
         System.out.println("wyplata:"+this.salary);
         System.out.println (new Date());
         this.salary = salary;
     }
-
-    public int getSalary() {
-        return salary;
+    public void sale(Human seller,Human buyer,Double price){
+       if (buyer.cash < price) {
+           System.out.println("Nie stać cie");
+       }else if (!buyer.hasFreeParkingLot()) {
+           System.out.println("Nie ma miejsca");
+       }else if (!seller.hasCar(this)) {
+           System.out.println("Nie ma aut na sprzedaz");
+       } else {
+           seller.removeCar(this);
+           buyer.addCar(this);
+           seller.cash += price;
+           buyer.cash -= price;
+           System.out.println("Transkacja udana");
+       }
     }
+    public void removeCar(Car){
+        for(int i = 0; i < garage.length; i++){
+            if(garage[i] == Car){
+                garage[i] = null;
+
+                public void addCar(Car){
+                    for(int i = 0; i < garage.length; i++){
+                        if(garage[i] == null){
+                            garage[i] = Car;
+
+    public boolean hasCar() {
+        for (int i = 0; i < garage.length; i++){
+            if (garage[i] == Car){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean hasFreeParkingLot() {
+        for (int i = 0; i < garage.length; i++){
+            if (garage[i] == null){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
 
 
